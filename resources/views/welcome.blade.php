7bc85c5609305fc7c8791e982d59033d3a10ba50
safaroque7 <faroque.computer@gmail.com>
2023-10-17 15:59:36 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+
+    <div class="container mx-auto">
+        <div class="flex bg-gray-500">
+            <div class="w-1/3">
+                <h1> S A Faroque </h1>
+            </div>
+
+            <div class="w-2/3">
+                <ul>
+                    <li> <a href="#"> Home </a> </li>
+                    <li> <a href="#"> Services </a> </li>
+                    <li> <a href="#"> Contact </a> </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
